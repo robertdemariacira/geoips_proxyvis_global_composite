fd381fb51a6a4ed8e7397c544dc4f18705d4f2f2
Robert DeMaria <robert.demaria@colostate.edu>
2024-12-16 12:52:44 -0700

    Renamed package.

--- my_package/_version.py
+++
@@ -1,7 +0,0 @@
-# # # This source code is protected under the license referenced at
-# # # https://github.com/NRLMMD-GEOIPS.
-
-# DO NOT EDIT
-# managed by poetry-dynamic-versioning
-__version__ = "0.0.0"
-__version_tuple__ = (0, 0, 0)
